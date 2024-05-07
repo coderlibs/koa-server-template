58031051bfabaa6565472f7052fe5ddeb41304b0
leiweisong <83460555@qq.com>
2024-05-07 10:32:43 +0800

    公共配置

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
+const moduleAlias = require('module-alias');
+moduleAlias.addAliases({
+  "@": __dirname + "/src",
+  "@db": __dirname + '/src/db',
+  "@utils": __dirname + "/src/utils",
+  "@model": __dirname + "/src/model",
+  "@assets": __dirname + "/src/assets",
+  "@config": __dirname + "/src/config",
+  "@service": __dirname + "/src/service",
+  "@middleware": __dirname + "/src/middleware",
+  "@controller": __dirname + "/src/controller"
+});
 const Koa = require('koa');
 const app = new Koa();
 const router = require("./src/routers/index.js");
