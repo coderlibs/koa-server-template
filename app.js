50fc2905c39b73b8e8515c2268638c1bf6693c58
leiweisong <83460555@qq.com>
2024-05-07 11:00:47 +0800

    配置动态端口

--- app.js
+++ app.js
@@ -10,10 +10,11 @@ moduleAlias.addAliases({
   "@middleware": __dirname + "/src/middleware",
   "@controller": __dirname + "/src/controller"
 });
+const { port } = require('@config/index.js');
 const Koa = require('koa');
 const app = new Koa();
 const router = require("./src/routers/index.js");
 app.use(router.routes(), router.allowedMethods()); // 启用路由，允许网络请求
-app.listen(5050, () => {
-    console.debug('Server is running at http://localhost:' + 5050);
+app.listen(port, () => {
+    console.debug('Server is running at http://localhost:' + port);
 });
